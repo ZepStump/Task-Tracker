1e16a019b0b766e804e6f6081d5803a23ab91d42
unknown <bertmorris911@gmail.com>
2023-04-18 14:06:25 -0700

    map list of tasks to task items

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,5 +1,28 @@
 import React from "react";
 
+const mockData = [
+  {
+    name: "Iterate through tasks data and create list of tisk items",
+    due: "04/19/23",
+    status: "in progress",
+  },
+  {
+    name: "create handle filter function",
+    due: "04/20/23",
+    status: "to-do",
+  },
+  {
+    name: "allow users to delete task items",
+    due: "04/19/13",
+    status: "to-do",
+  },
+  {
+    name: "allow users to edit task items",
+    due: "04/19/23",
+    status: "to-do",
+  },
+];
+
 function Tasks() {
   const Header = () => {
     return (
@@ -10,18 +33,20 @@ function Tasks() {
       </div>
     );
   };
-  const Tasks = () => {
+  const TaskItems = (props) => {
     return (
       <div className="tasks">
         <h2 className="tasks__title">Tasks</h2>
         <ul className="tasks__list">
-          <li className="tasks__list-item">
-            <span className="tasks__task-name">Task</span>
-            <span>Status</span>
-            <span>Due</span>
-            <button>Edit</button>
-            <button>Delete</button>
-          </li>
+          {props.taskData.map((task) => (
+            <li className="tasks__list-item" key="task.name">
+              <span className="tasks__task-name">{task.name}</span>
+              <span>{task.status}</span>
+              <span>{task.due}</span>
+              <button>Edit</button>
+              <button>Delete</button>
+            </li>
+          ))}
         </ul>
       </div>
     );
@@ -30,7 +55,7 @@ function Tasks() {
   return (
     <div className="main">
       <Header />
-      <Tasks />
+      <TaskItems taskData={mockData} />
     </div>
   );
 }
