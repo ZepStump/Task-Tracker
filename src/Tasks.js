fac63a8dd307f552d83d7ce0797b8448db8aab6c
BertMorris <bertmorris911@gmail.com>
2023-04-19 14:01:28 -0700

    First push with firebase ingrated. All basic functions should be operational.

--- src/Tasks.js
+++ src/Tasks.js
@@ -3,53 +3,22 @@ import { useStateValue } from "./StateProvider";
 import { auth, db } from "./firebase-setup/firebase";
 import { Link } from "react-router-dom";
 
-// mock data for dev
-const mockTasks = [
-  {
-    name: "Iterate through tasks data and create list of tisk items",
-    due: "04/19/23",
-    status: "in progress",
-    description: "this area will contain a description of the task",
-  },
-  {
-    name: "create handle filter function",
-    due: "04/20/23",
-    status: "to-do",
-    description: "this area will contain a description of the task",
-  },
-  {
-    name: "allow users to delete task items",
-    due: "04/19/13",
-    status: "to-do",
-    description: "this area will contain a description of the task",
-  },
-  {
-    name: "allow users to edit task items",
-    due: "04/19/23",
-    status: "to-do",
-    description: "this area will contain a description of the task",
-  },
-];
-
 export default function Tasks() {
+  //for user auth purposes
+  const [{ user }, dispatch] = useStateValue();
   const [name, setName] = useState("No User");
-  const [tasks, setTasks] = useState(mockTasks);
+  const [tasks, setTasks] = useState([]);
   const [searchPhrase, setSearchPhrase] = useState("");
   const [filteredTasks, setFilteredTasks] = useState([]);
   const [toggleAddTask, setToggleAddTask] = useState(false);
+  const [editTask, setEditTask] = useState(null);
   // init filtered task for search functionality
   useEffect(() => {
     const results = tasks.filter((task) =>
-      task.name.toLowerCase().includes(searchPhrase.toLowerCase())
+      task.data.name.toLowerCase().includes(searchPhrase.toLowerCase())
     );
     setFilteredTasks(results);
   }, [searchPhrase, tasks]);
-  // user placeholder
-  const user = "Test User";
-  // init user
-  useEffect(() => {
-    setName(user);
-  }, []);
   // user log out
   const signOut = () => {
     if (user) {
@@ -57,71 +26,23 @@ export default function Tasks() {
     }
   };
 
-  // const [name, setName] = useState("No User");
-  // const [tasks, setTasks] = useState([]);
-  // const [taskName, setTaskName] = useState("");
-  // const [taskText, setTaskText] = useState("");
-  // const [taskDue, setTasDue] = useState("");
-  // const [taskStatus, setTaskStatus] = useState("");
-
-  // //this should retireve names and tasks
-  // useEffect(() => {
-  //   db.collection("user_names").doc(user.uid)
-  //       .onSnapshot(doc => setName(doc.data().name));
-  //   db.collection("users").doc(user.uid).collection("tasks")
-  //       .onSnapshot(snapshot => (
-  //           setTasks(snapshot.docs.map(doc => ({
-  //             name: doc.id,
-  //             data: doc.data()
-  //           })))
-  //       ))
-  // }, [])
-
-  // const add = () => {
-
-  //   db
-  //     .collection("users")
-  //     .doc(user?.uid)
-  //     .collection("tasks")
-  //     .doc(taskName)
-  //     .set({
-  //       name: taskName,
-  //       due: taskDue,
-  //       text: taskText,
-  //       status: taskStatus
-  //     });
-
-  //     alert("Task was created!");
-  // }
-
-  // const edit = () => {
-
-  //   db
-  //     .collection("users")
-  //     .doc(user?.uid)
-  //     .collection("tasks")
-  //     .doc(taskName)
-  //     .update({
-  //       name: taskName,
-  //       due: taskDue,
-  //       text: taskText,
-  //       status: taskStatus
-  //     });
-
-  //     alert("Task was updated!");
-  // }
-
-  // const remove = () => {
-
-  //   db
-  //     .collection("users")
-  //     .doc(user?.uid)
-  //     .collection("tasks")
-  //     .doc(taskName)
-  //     .delete();
-
-  //     alert("Task was deleted!");
-  // }
+  // //this should retrieve names and tasks
+  useEffect(() => {
+    db.collection("user_names")
+      .doc(user.uid)
+      .onSnapshot((doc) => setName(doc.data().name));
+    db.collection("users")
+      .doc(user.uid)
+      .collection("tasks")
+      .onSnapshot((snapshot) =>
+        setTasks(
+          snapshot.docs.map((doc) => ({
+            name: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
 
   return (
     <div className="main">
@@ -133,14 +54,22 @@ export default function Tasks() {
           setSearchPhrase={setSearchPhrase}
           setToggleAddTask={setToggleAddTask}
         />
-        <TasksItems filteredTasks={filteredTasks} />
+        <TasksItems filteredTasks={filteredTasks} setEditTask={setEditTask} />
       </TasksLayout>
       {/* logic allows add task form to be opened and closed */}
       {toggleAddTask && (
         <AddTaskForm
+          user={user}
           tasks={tasks}
           setTasks={setTasks}
           setToggleAddTask={setToggleAddTask}
+        />
+      )}
+      {editTask && (
+        <EditTaskForm
+          user={user}
+          editTask={editTask}
+          setEditTask={setEditTask}
         />
       )}
     </div>
@@ -183,8 +112,8 @@ function TasksHeader({
     setFilteredTasks((tasks) => {
       let newTasks = [...tasks];
       newTasks.sort((a, b) => {
-        const aLower = a.name.toLowerCase();
-        const bLower = b.name.toLowerCase();
+        const aLower = a.data.name.toLowerCase();
+        const bLower = b.data.name.toLowerCase();
         if (aLower < bLower) {
           return -1;
         } else if (aLower > bLower) {
@@ -201,8 +130,8 @@ function TasksHeader({
     setFilteredTasks((tasks) => {
       let newTasks = [...tasks];
       newTasks.sort((a, b) => {
-        const aLower = a.status.toLowerCase();
-        const bLower = b.status.toLowerCase();
+        const aLower = a.data.status.toLowerCase();
+        const bLower = b.data.status.toLowerCase();
         if (aLower < bLower) {
           return -1;
         } else if (aLower > bLower) {
@@ -214,13 +143,13 @@ function TasksHeader({
       return newTasks;
     });
   };
-  // sorts tasks by due dat into filteredTasks
+  // sorts tasks by due date into filteredTasks
   const sortDue = () => {
     setFilteredTasks((tasks) => {
       let newTasks = [...tasks];
       newTasks.sort((a, b) => {
-        const aLower = a.due.toLowerCase();
-        const bLower = b.due.toLowerCase();
+        const aLower = a.data.due.toLowerCase();
+        const bLower = b.data.due.toLowerCase();
         if (aLower < bLower) {
           return -1;
         } else if (aLower > bLower) {
@@ -271,50 +200,107 @@ function TasksHeader({
   );
 }
 // task items content
-function TasksItems({ filteredTasks }) {
+function TasksItems({ user, filteredTasks, setEditTask }) {
+  // handle edit task
+  const handleEdit = (taskName) => {
+    console.log({ taskName });
+    setEditTask(taskName);
+  };
+  // delete task from db
+  const remove = (task) => {
+    console.log(task);
+    db.collection("users")
+      .doc(user?.uid)
+      .collection("tasks")
+      .doc(task.name)
+      .delete();
+
+    alert("Task was deleted!");
+  };
+  // handle delete task from db
+  const handleDelete = (task) => {
+    console.log(`Delete task: ${task.name}`);
+    remove(task);
+  };
+  // reformats date from form
+  const formatDate = (date) => {
+    return new Date(date).toLocaleDateString();
+  };
   return (
     <ul className="tasks__list">
       {filteredTasks.map((task) => (
         <li className="tasks__list-item" key={task.name}>
           <div className="tasks__item-main">
-            <span className="tasks__task-name">{task.name}</span>
-            <span>{task.status}</span>
-            <span>{task.due}</span>
-            <button>Edit</button>
-            <button>Delete</button>
+            <span className="tasks__task-name">{task.data.name}</span>
+            <span>{task.data.status}</span>
+            <span>{formatDate(task.data.due)}</span>
+            <button
+              type="button"
+              key={task.name}
+              onClick={() => handleEdit(task)}
+            >
+              Edit
+            </button>
+            <button type="button" onClick={() => handleDelete(task)}>
+              Delete
+            </button>
           </div>
-          <span className="tasks__task-description">{task.description}</span>
+          <span className="tasks__task-description">{task.data.text}</span>
         </li>
       ))}
     </ul>
   );
 }
 // pop out form to add new task
-// see no added value by using controlled inputs
-function AddTaskForm({ tasks, setTasks, setToggleAddTask }) {
-  // reformats date from form
-  const formatDate = (date) => {
-    return new Date(date).toLocaleDateString();
+function AddTaskForm({ user, tasks, setTasks, setToggleAddTask }) {
+  console.log(`User.uid ${user.uid}`);
+  // init new task state
+  const [newTask, setNewTask] = useState({
+    name: "",
+    due: "",
+    description: "",
+    status: "to-do",
+  });
+  // handle update of form input
+  const handleChange = (e) => {
+    const { name, value } = e.currentTarget;
+    setNewTask({
+      ...newTask,
+      [name]: value,
+    });
+  };
+
+  // append new task to db
+  const add = (task) => {
+    console.log(`Add user: ${user}`);
+    db.collection("users")
+      .doc(user?.uid)
+      .collection("tasks")
+      .doc(task.name)
+      .set({
+        name: task.name,
+        due: task.due,
+        text: task.description,
+        status: task.status,
+      });
+
+    alert("Task was created!");
   };
   // appends new task to tasks
   const handleSubmitTask = (e) => {
     e.preventDefault();
-    setTasks([
-      ...tasks,
-      {
-        name: e.currentTarget.name.value,
-        due: formatDate(e.currentTarget.due.value),
-        status: e.currentTarget.status.value,
-        description: e.currentTarget.description.value,
-      },
-    ]);
+    add(newTask);
     // closes new task form
     setToggleAddTask(false);
   };
 
   return (
     <div className="form__container">
-      <button type="button" onClick={() => setToggleAddTask(false)}>
+      <button
+        className="form__close-btn"
+        type="button"
+        onClick={() => setToggleAddTask(false)}
+      >
         Close
       </button>
       <form
@@ -324,14 +310,40 @@ function AddTaskForm({ tasks, setTasks, setToggleAddTask }) {
       >
         <h2 className="form__title">New Task</h2>
         <label htmlFor="name">Task</label>
-        <input type="text" id="name" name="name" required></input>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={(e) => handleChange(e)}
+          value={newTask.name}
+          required
+        ></input>
         <label htmlFor="due">Due Date</label>
-        <input type="date" id="for" name="due" required></input>
+        <input
+          type="date"
+          id="for"
+          name="due"
+          onChange={(e) => handleChange(e)}
+          value={newTask.due}
+          required
+        ></input>
         <label htmlFor="description">Description</label>
-        <textarea id="description" name="description"></textarea>
+        <textarea
+          id="description"
+          name="description"
+          onChange={(e) => handleChange(e)}
+          value={newTask.description}
+        ></textarea>
         <div className="form__radio-group">
           <div>
-            <input type="radio" id="to-do" name="status" value="to-do"></input>
+            <input
+              type="radio"
+              id="to-do"
+              name="status"
+              value="to-do"
+              checked={newTask.status === "to-do"}
+              onChange={(e) => handleChange(e)}
+            ></input>
             <label htmlFor="to-do">To-do</label>
           </div>
           <div>
@@ -340,6 +352,8 @@ function AddTaskForm({ tasks, setTasks, setToggleAddTask }) {
               id="in-progress"
               name="status"
               value="in-progress"
+              checked={newTask.status === "in-progress"}
+              onChange={(e) => handleChange(e)}
             ></input>
             <label htmlFor="in-progress">In Progress</label>
           </div>
@@ -349,13 +363,131 @@ function AddTaskForm({ tasks, setTasks, setToggleAddTask }) {
               id="complete"
               name="status"
               value="complete"
+              checked={newTask.status === "complete"}
+              onChange={(e) => handleChange(e)}
+            ></input>
+            <label htmlFor="complete">Complete</label>
+          </div>
+        </div>
+        <button className="tasks__new-btn" type="submit" form="add-task">
+          Add Task
+        </button>
+      </form>
+    </div>
+  );
+}
+
+function EditTaskForm({ user, editTask, setEditTask }) {
+  const [updatedTask, setUpdatedTask] = useState(editTask);
+  // handle update of form input
+  const handleChange = (e) => {
+    const { name, value } = e.currentTarget;
+    setUpdatedTask({
+      ...updatedTask,
+      data: { ...updatedTask.data, [name]: value },
+    });
+  };
+  // reformats date from form
+  const formatDate = (date) => {
+    return new Date(date).toLocaleDateString();
+  };
+  // update task data on db
+  const edit = (task) => {
+    db.collection("users")
+      .doc(user?.uid)
+      .collection("tasks")
+      .doc(task.name)
+      .update({
+        name: task.data.name,
+        due: formatDate(task.data.due),
+        text: task.data.text,
+        status: task.data.status,
+      });
+
+    alert("Task was updated!");
+  };
+  // handle update task
+  const handleSubmitEdit = (e) => {
+    e.preventDefault();
+    edit(updatedTask);
+    // closes edit task form
+    setEditTask(null);
+  };
+
+  return (
+    <div className="form__container">
+      <button type="button" onClick={() => setEditTask(null)}>
+        Cancel
+      </button>
+      <form
+        className="form"
+        id="edit-task"
+        onSubmit={(e) => handleSubmitEdit(e)}
+      >
+        <h2 className="form__title">Edit Task</h2>
+        <label htmlFor="name">Task</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={(e) => handleChange(e)}
+          value={updatedTask.data.name}
+          required
+        ></input>
+        <label htmlFor="due">Due Date</label>
+        <input
+          type="date"
+          id="for"
+          name="due"
+          onChange={(e) => handleChange(e)}
+          value={updatedTask.data.due}
+          required
+        ></input>
+        <label htmlFor="text">Description</label>
+        <textarea
+          id="text"
+          name="text"
+          onChange={(e) => handleChange(e)}
+          value={updatedTask.data.text}
+        ></textarea>
+        <div className="form__radio-group">
+          <div>
+            <input
+              type="radio"
+              id="to-do"
+              name="status"
+              value="to-do"
+              checked={updatedTask.data.status === "to-do"}
+              onChange={(e) => handleChange(e)}
+            ></input>
+            <label htmlFor="to-do">To-do</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="in-progress"
+              name="status"
+              value="in-progress"
+              checked={updatedTask.data.status === "in-progress"}
+              onChange={(e) => handleChange(e)}
+            ></input>
+            <label htmlFor="in-progress">In Progress</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="complete"
+              name="status"
+              value="complete"
+              checked={updatedTask.data.status === "complete"}
+              onChange={(e) => handleChange(e)}
             ></input>
             <label htmlFor="complete">Complete</label>
           </div>
         </div>
 
-        <button className="tasks__new-btn" type="submit" form="add-task">
-          Add Task
+        <button className="tasks__new-btn" type="submit" form="edit-task">
+          Save Edit
         </button>
       </form>
     </div>
