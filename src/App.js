2f2f14f7581f8a940b0167f2b158e856a8facb4f
BertMorris <bertmorris911@gmail.com>
2023-04-21 12:49:31 -0700

    change browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/Task-Tracker" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/tasks" element={<Tasks />} />
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { StateProvider } from "./StateProvider";
 import reducer, { initialState } from "./reducer";
 
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StateProvider initialState={initialState} reducer={reducer}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </StateProvider>
   </React.StrictMode>
 );
